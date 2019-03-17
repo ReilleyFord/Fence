7e46b18a8092a1c2333128b5637670e94af912fa
Reilley <reilleyford@gmail.com>
2019-03-17 14:39:38 -0400

    Added renaming functionality

--- fence.cpp
+++ fence.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include <filesystem>
 
 namespace fs = std::filesystem;
@@ -14,30 +15,52 @@ const string getInput(string &question) {
   return output;
 }
 
+const vector<fs::path> getFiles(string &directory) {
+  vector<fs::path> files;
+  try {
+    for(fs::path const &file : fs::directory_iterator(directory)) {
+      string ext = file.extension();
+      if(ext == ".mp4" || ext == ".mkv"){
+        files.push_back(file.filename());
+      }
+    }
+    sort(files.begin(), files.end());
+  } catch(exception &e) {
+    cout << e.what() << endl;
+  }
+  return files;
+}
+
+const void renameFiles(string &ext, string &show, vector<fs::path> files) {
+  int i = 0;
+  for(fs::path &file : files) {
+    i++;
+    string name;
+    if(i >= 10) {
+      name = show + "E" + to_string(i);
+    } else {
+      name = show + "E0" + to_string(i);
+    }
+    file.replace_filename(name);
+    file.replace_extension(ext);
+    cout << file.filename() << endl;
+  }
+}
+
 int main() {
 
-  string inputDir;
-
   string question = "Input Directory: ";
-  inputDir = getInput(question);
+  string inputDir = getInput(question);
 
-  question = "Filetype (mkv or mp4):"
+  question = "Filetype (mkv or mp4):";
   string extension = getInput(question);
 
   question = "Show and season (eg. Seinfeld S01)";
   string show = getInput(question);
 
-  try {
-    for(auto const &file : fs::directory_iterator(inputDir)) {
-      string ext = file.path().extension();
-      if(ext == ".mp4" || ext == ".mkv"){
-        cout << file.path().filename() << endl;
-      }
-    }
-  } catch(exception &e) {
-    cout << e.what() << endl;
-  }
+  vector<fs::path> files = getFiles(inputDir);
 
+  renameFiles(extension, show, files);
 
   cin.clear();
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
